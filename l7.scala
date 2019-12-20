dbe1a143229a9d8cb0fcc1aff148d59c6f6d801f
Krattos201 <36298698+Krattos201@users.noreply.github.com>
2019-12-20 23:27:33 +0530

    Update l7.scala

--- l7.scala
+++ l7.scala
@@ -1,24 +1,19 @@
 import scala.io.Source
- 
-object WordCount extends App {
- 
-  //val url = Source.fromFile("file.txt")
-  val header = "Rank Word  Frequency\n==== ======== ======"
- 
-  def wordCnt =
-    Source.fromFile("file.txt").getLines()
-      .filter(_.nonEmpty)
-      .flatMap(_.split("""\W+""")).toSeq
-      .groupBy(_.toLowerCase())
-      .mapValues(_.size).toSeq
-      .sortWith { case ((_, v0), (_, v1)) => v0 > v1 }
-      .take(10).zipWithIndex
- 
-  println(header)
-  wordCnt.foreach {
-    case ((word, count), rank) => println(f"${rank + 1}%4d $word%-8s $count%6d")
+object scalawordcount {
+  def main(args:Array[String]){
+    if(args.length!=1){
+      System.err.println("Usage: sbt run \"filepath\"")
+      System.exit(0)
+    }
+    var fn = args(0)
+    var wc = scala.collection.mutable.Map[String,Int]()
+    for(line<-Source.fromFile(fn).getLines)
+      for(word<-line.split(" "))
+        wc(word) = if(wc.contains(word))
+                wc(word)+1
+              else 1
+    for((k,v)<-wc)
+      printf("Word %s occurs %d time\n",k,v)
+      println(wc)
   }
- 
-  println(s"\nSuccessfully completed without errors. [total ${scala.compat.Platform.currentTime - executionStart} ms]")
- 
 }
